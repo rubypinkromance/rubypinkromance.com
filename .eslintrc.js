c99f88b1cf410bcb2469c5a140443d4d949b5dfd
Ruby Pink <hello@rubypinkromance.com>
2024-02-05 18:54:56 -0800

    add config files

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2023: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
