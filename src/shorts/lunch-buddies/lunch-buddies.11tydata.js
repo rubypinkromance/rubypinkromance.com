e5c37e95d0d05f18ac743fc5834e6b0b66a20c4e
Ruby Pink <hello@rubypinkromance.com>
2024-03-08 12:33:07 -0800

    add bundle promo

--- src/shorts/lunch-buddies/lunch-buddies.11tydata.js
+++ src/shorts/lunch-buddies/lunch-buddies.11tydata.js
@@ -6,4 +6,5 @@ module.exports = {
   seriesCategory: 'Office Sex',
   seriesExcerpt: 'Ash helps Tony relieve an awkward erection in her office.',
   social_image: 'social/lunch-buddies-social.jpg',
+  bundled_in: ['lunch-buddies'],
 };
