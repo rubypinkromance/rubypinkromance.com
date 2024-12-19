9bee28326601f5d7504b7558fe55b5e13af869b6
Ruby Pink <hello@rubypinkromance.com>
2024-12-19 10:14:00 -0800

    add stacys mom

---
+++ src/shorts/stacys-family/stacys-family.11tydata.js
@@ -0,0 +1,7 @@
+export default {
+  series: 'stacys-family',
+  seriesTitle: 'Stacy’s Family',
+  category: 'Taboo',
+  seriesCategory: 'Taboo',
+  seriesExcerpt: 'Stacy’s family is full of secrets.',
+};
